08a064fc4731fc6c2138eb0630031ba98aaef537
ppatra126 <pratyushp126@gmail.com>
2025-04-26 21:04:31 -0500

    updated model links

--- functions/process_data.py
+++ functions/process_data.py
@@ -5,9 +5,7 @@ import os
 def download_process_data(path="colab_demo"):
     os.makedirs(path, exist_ok=True)
     print("Downloading data")
-    torch.hub.download_url_to_file('https://image-editing-test-12345.s3-us-west-2.amazonaws.com/colab_examples/lsun_bedroom1.pth', os.path.join(path, 'lsun_bedroom1.pth'))
-    torch.hub.download_url_to_file('https://image-editing-test-12345.s3-us-west-2.amazonaws.com/colab_examples/lsun_bedroom2.pth', os.path.join(path, 'lsun_bedroom2.pth'))
-    torch.hub.download_url_to_file('https://image-editing-test-12345.s3-us-west-2.amazonaws.com/colab_examples/lsun_bedroom3.pth', os.path.join(path, 'lsun_bedroom3.pth'))
-    torch.hub.download_url_to_file('https://image-editing-test-12345.s3-us-west-2.amazonaws.com/colab_examples/lsun_edit.pth', os.path.join(path, 'lsun_edit.pth'))
-    torch.hub.download_url_to_file('https://image-editing-test-12345.s3-us-west-2.amazonaws.com/colab_examples/lsun_church.pth', os.path.join(path, 'lsun_church.pth'))
+    torch.hub.download_url_to_file('https://heibox.uni-heidelberg.de/f/b95206528f384185889b/?dl=1', os.path.join(path, 'lsun_bedroom.pth'))
+    torch.hub.download_url_to_file('https://heibox.uni-heidelberg.de/f/0701aac3aa69457bbe34/?dl=1', os.path.join(path, 'lsun_cat.pth'))
+    torch.hub.download_url_to_file('https://heibox.uni-heidelberg.de/f/44ccb50ef3c6436db52e/?dl=1', os.path.join(path, 'lsun_church.pth'))
     print("Data downloaded")
